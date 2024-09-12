88021913ff17ba8b832f57eb87d061b7f7d0cd03
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-12 18:10:47 +0200

    first commit

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from '../user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
@@ -8,13 +10,22 @@ import { UserService } from '../user.service';
       <li *ngFor="let user of users">{{ user.name }} ({{ user.age }})</li>
     </ul>
   `,
-  styles: [`
-    ul { list-style-type: none; padding: 0; }
-    li { margin-bottom: 10px; }
-  `]
+  styles: [
+    `
+      ul {
+        list-style-type: none;
+        padding: 0;
+      }
+      li {
+        margin-bottom: 10px;
+      }
+    `,
+  ],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class UserListComponent implements OnInit {
-  users: any[];
+  users: User[];
 
   constructor(private userService: UserService) {}
 
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private users = [{name: 'John', age: 30}, {name: 'Jane', age: 25}];
-
-  constructor() { }
+  private users = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+  ];
 
   getUsers() {
     return this.users;
   }
 
-  addUser(user) {
+  addUser(user: { name: string; age: number }) {
     this.users.push(user);
   }
 }
